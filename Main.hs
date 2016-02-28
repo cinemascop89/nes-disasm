86db6da164fb898317dd9125261a06c62b218673
ed <ed.cinemascop@yahoo.es>
2016-02-28 16:22:52 -0300

    get rom name in args

--- Main.hs
+++ Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import System.Environment
 import Control.Monad.State
 import qualified Data.ByteString as BS
 
@@ -9,7 +10,8 @@ import DisAsm
 
 main :: IO ()
 main = do
-     rom <- BS.readFile "smb.nes"
+     romName:_ <- getArgs
+     rom <- BS.readFile romName
      let header = parseHeader rom
      let prgBankData = take (prgBanks header*0x4000) (drop 0x10 (BS.unpack rom))
      let disasm = initDisassembler prgBankData
